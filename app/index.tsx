242fbc4d4d427b18c2240e0bc9009df275a220da
penny0102 <mohammedlelly2006@gmail.com>
2026-06-09 18:59:29 +0200

    update

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import { Text, View, StyleSheet } from "react-native";
-import {Zap } from "lucide-react-native";
+import {Zap, Footprints, MapPinned, Flame } from "lucide-react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
+import * as Progress from 'react-native-progress';
 
 export default function Index() {
   return (
@@ -14,16 +15,67 @@ export default function Index() {
       }}
     >
       <View style={{flexDirection: "row", gap:7}}>
-        <Zap color="#22C55E"/>
-        <Text style={{color: "#22C55E", fontFamily: "Sprintura", fontSize: 20}}>
-          Performanes
-        </Text>
-      </View>
-      <View style={styles.cardStyle}>
-        <Text>
+          <Zap color="#22C55E"/>
+          <Text style={{color: "#22C55E", fontFamily: "Sprintura", fontSize: 20}}>
+            Performanes
+          </Text>
+        </View>
+        <View style={styles.cardStyle}>
+          <View style={{flexDirection: "row", gap: 10}}>
+            <Footprints color="#9b9b9b" size={20}/>
+            <Text style={{color: "#9b9b9b"}}>
+              STEPS TODAY
+            </Text>
+            
+          </View>
+          <Text style={{fontSize: 50, color: "#22C55E"}}>
+              4522,5
+          </Text>
+            <View style={{paddingTop: 20}}>
+              <Progress.Bar
+                progress={0.5}
+                width={300}
+                height={12}
+                borderRadius={10}
+                color="#22C55E"
+              />
+              <Text style={{color: "#22C55E", paddingTop: 10}}>
+                42%  Completed
+              </Text>
+            </View>
+        </View>
+        <View style={{flexDirection: "row", flexWrap: "wrap", gap: 35}}>
+          <View style={styles.styleMiniCard}>
+              <MapPinned color="#63a9f9" size={20}/>
+              <Text style={{paddingTop: 5, color: "#9b9b9b"}}>
+                Distance
+              </Text>
+              <View style={{flexDirection: "row", gap: 7}}>
+                <Text style={{fontSize: 30, color: "#d4d4d4"}}>
+                  32 
+                </Text>
+                <Text style={{paddingTop: 16}}>
+                  km
+                </Text>
+              </View>
+          </View>
+          <View style={styles.styleMiniCard}>
+              <Flame color="#f98163" size={20}/>
+              <Text style={{paddingTop: 5, color: "#9b9b9b"}}>
+                Calories
+              </Text>
+              <View style={{flexDirection: "row", gap: 7}}>
+                <Text style={{fontSize: 30, color: "#d4d4d4"}}>
+                  240 
+                </Text>
+                <Text style={{paddingTop: 16}}>
+                  kcal
+                </Text>
+              </View>
+          </View>
           
-        </Text>
-      </View>
+        </View>
+          
     </SafeAreaView>
   );
 }
@@ -32,7 +84,7 @@ export default function Index() {
 
 const styles = StyleSheet.create({
   cardStyle: {
-    backgroundColor: "#1e293b",
+      backgroundColor: "#1e293b",
       // opacity: 0.5
       borderRadius: 20,
       borderWidth: 1,
@@ -42,11 +94,27 @@ const styles = StyleSheet.create({
       alignSelf: "center",
       paddingHorizontal: 15,
       paddingTop: 10,
-      paddingBottom: 40,
+      paddingBottom: 15,
       shadowColor: "#000",
       shadowOffset: {width: 2, height: 2},
       shadowOpacity: 3.0,
       shadowRadius: 4,
       elevation: 100
+  },
+  styleMiniCard: {
+    backgroundColor: "#1e293b",
+    marginTop:  20,
+    borderRadius: 15,
+    width: "45%",
+    paddingTop: 10,
+    paddingHorizontal: 15,
+    paddingBottom: 15,
+    shadowColor: "#000",
+    shadowOffset: {width: 2, height: 2},
+    shadowOpacity: 3.0,
+    shadowRadius: 4,
+    elevation: 100
+    
+    
   }
 })
