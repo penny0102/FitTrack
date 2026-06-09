236498fd3a0079d4d9b23ec85bc711ea66d1bad1
penny0102 <mohammedlelly2006@gmail.com>
2026-06-09 15:58:44 +0200

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
 
 export default function RootLayout() {
-  return <Stack />;
+   const [fontsLoaded] = useFonts({
+      Sprintura: require("../assets/font/LTInternet-Regular.ttf"),
+    });
+  
+    if (!fontsLoaded) return null;
+  return <Stack screenOptions={{headerShown: false}}/>;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,52 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import {Zap } from "lucide-react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Index() {
   return (
-    <View
+    <SafeAreaView
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        paddingHorizontal: 20,
+        paddingTop: 20,
+        backgroundColor: "#0f172a"
+
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <View style={{flexDirection: "row", gap:7}}>
+        <Zap color="#22C55E"/>
+        <Text style={{color: "#22C55E", fontFamily: "Sprintura", fontSize: 20}}>
+          Performanes
+        </Text>
+      </View>
+      <View style={styles.cardStyle}>
+        <Text>
+          
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
+
+
+
+const styles = StyleSheet.create({
+  cardStyle: {
+    backgroundColor: "#1e293b",
+      // opacity: 0.5
+      borderRadius: 20,
+      borderWidth: 1,
+      borderColor: 'black',
+      marginTop: 50,
+      width: "100%",
+      alignSelf: "center",
+      paddingHorizontal: 15,
+      paddingTop: 10,
+      paddingBottom: 40,
+      shadowColor: "#000",
+      shadowOffset: {width: 2, height: 2},
+      shadowOpacity: 3.0,
+      shadowRadius: 4,
+      elevation: 100
+  }
+})
